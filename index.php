ee6835c36e9013fec3fab00f9472100e0c71c3f5
parikshit <vanjariparikshit@gmail.com>
2023-03-04 10:06:38 +0530

    no changes done

--- index.php
+++ index.php
@@ -113,37 +113,7 @@ else{
       </div>
     </div>
   </div>
-
-  <?php
-  // if($insert){
-  //   echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-  //   <strong>Success!</strong> Your note has been inserted successfully
-  //   <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-  //     <span aria-hidden='true'>×</span>
-  //   </button>
-  // </div>";
-  // }
-  ?>
-     <?php
-  // if($delete){
-  //   echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-  //   <strong>Success!</strong> Your note has been deleted successfully
-  //   <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-  //     <span aria-hidden='true'>×</span>
-  //   </button>
-  // </div>";
-  // }
-  // ?>
-    <?php
-  // if($update){
-  //   echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-  //   <strong>Success!</strong> Your note has been updated successfully
-  //   <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-  //     <span aria-hidden='true'>×</span>
-  //   </button>
-  // </div>";
-  // }
-  // ?>
+  
   <div class="container my-4">
     <h2>To Do List</h2>
     <form action="/crud/index.php" method="POST">
